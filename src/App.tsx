817a2ec5c8c9d6f06ef7728b22199f3e647a5b83
Konstantinos Ntavarinos <ntavarinos.k@gmail.com>
2024-02-29 01:24:42 +0200

    About section + css changes on Home section

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import './App.css'
-import React from "react";
+import './App.module.css'
 import About from "./components/About/About.tsx";
 import Home from "./components/Home/Home.tsx";
 import Work from "./components/Work/Work.tsx";
 import Contact from "./components/Contact/Contact.tsx";
+import styles from "./App.module.css";
 
 function App() {
 
   return (
-    <>
+    <div className={styles.container}>
       <Home />
       <About />
       <Work />
       <Contact />
-    </>
+    </div>
   )
 }
 
--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import styles from "../About/About.module.css";
+import strings from "../../assets/context/strings.ts";
+import aboutImage from '../../assets/images/about-cleaning-crew.png';
+
 
 const About = () => {
     return (
-        <div>About</div>
+        <div className={styles.section}>
+            <div className={styles.container}>
+                <div className={styles.left}>
+                    <img src={aboutImage} alt="cleaning crew" className={styles.aboutImage}/>
+                </div>
+                <div className={styles.right}>
+                    <h1>{strings.aboutHeader}</h1>
+                    <p>{strings.aboutDescription_1}</p>
+                    <p>{strings.aboutDescription_2}</p>
+                    <p>{strings.aboutDescription_3}</p>
+                </div>
+            </div>
+        </div>
     );
 };
 
